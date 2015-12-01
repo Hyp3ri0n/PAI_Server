219cdcb3c6187e34900bbdf80979e0bd8e17ee28
ortizlu <ortizlu@127.0.0.1>
2015-12-01 09:47:16 +0100

    squelette client

--- PAI_Server/src/client.c
+++ PAI_Server/src/client.c
@@ -3,9 +3,22 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-
-void main()
+// ./client -p XXXX -s SERVER
+int main(int argc, char* argv[])
 {
-	int id_socket_client = socket(AF_INET, SOCK_STREAM, 0);
-	//int id_connect_client = connect(id_socket_client, X, sizeof(X));
+	if(argc < 5)
+	{
+		sprintf("Vous n'avez pas saisie toutes les informations nécessaire ! ");
+	}
+	else
+	{
+		int port = argv[2];
+		char* server = argv[4];
+		struct hostent* hostinfos = gethostbyname(server);
+
+		int id_socket_client = socket(AF_INET, SOCK_STREAM, 0);
+		//int id_connect_client = connect(id_socket_client, X, sizeof(X));
+	}
+
+	return 0;
 }
