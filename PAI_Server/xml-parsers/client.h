9ec3aeeb4058cb72a11d6a03ff42f233086967c3
ortizlu <ortizlu@127.0.0.1>
2015-12-03 08:34:34 +0100

    .h

---
+++ PAI_Server/xml-parsers/client.h
@@ -0,0 +1,14 @@
+/*
+ * client.h
+ *
+ *  Created on: 3 déc. 2015
+ *      Author: ortizlu
+ */
+
+#ifndef XML_PARSERS_CLIENT_H_
+#define XML_PARSERS_CLIENT_H_
+
+
+
+
+#endif /* XML_PARSERS_CLIENT_H_ */
